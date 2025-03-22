a6587fbb7db0ceb1a3e3d3f47c340fcaa26d04e2
Denis-G25 <gabordenis1@gmail.com>
2025-03-22 22:00:55 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        System.out.println("Suma a două numere: " + Suma.suma("3", "5"));  
+        System.out.println("Suma a trei numere: " + Suma.suma("3", "5", "2"));  
+        System.out.println("Suma a patru numere: " + Suma.suma("3", "5", "2", "4"));  
+    }
+}
+
---
+++ Suma.java
@@ -0,0 +1,20 @@
+
+public class Suma {
+    
+    public static String suma(String a, String b) {
+        int rezultat = Integer.parseInt(a) + Integer.parseInt(b);
+        return String.valueOf(rezultat);
+    }
+
+   
+    public static String suma(String a, String b, String c) {
+        int rezultat = Integer.parseInt(a) + Integer.parseInt(b) + Integer.parseInt(c);
+        return String.valueOf(rezultat);
+    }
+
+    
+    public static String suma(String a, String b, String c, String d) {
+        int rezultat = Integer.parseInt(a) + Integer.parseInt(b) + Integer.parseInt(c) + Integer.parseInt(d);
+        return String.valueOf(rezultat);
+    }
+}
